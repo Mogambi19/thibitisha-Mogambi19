0addd084d356e22eb48a9625bd3dd6a33a58e781
itsmo <itsmogambi19@gmail.com>
2025-11-25 17:44:12 +0300

    Lab 7.2

---
+++ route_menus.php
@@ -0,0 +1,12 @@
+<?php
+// Route menus for sidebar and controllers
+return [
+    'users' => [
+        'controller' => 'UserController',
+        'route' => 'users.index',
+    ],
+    'settings' => [
+        'controller' => 'SettingsController',
+        'route' => 'settings.index',
+    ],
+];
